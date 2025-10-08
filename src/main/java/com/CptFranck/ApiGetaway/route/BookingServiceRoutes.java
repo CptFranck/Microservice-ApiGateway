12b08da5f6f25e140c7c75f58972b7ba0e98a8ab
CptFranck <francois.caillet71@gmail.com>
2025-10-08 17:28:05 +0200

    Add BookingServiceRoutes

---
+++ src/main/java/com/CptFranck/ApiGetaway/route/BookingServiceRoutes.java
@@ -0,0 +1,28 @@
+package com.CptFranck.ApiGetaway.route;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.function.RequestPredicates;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.ServerResponse;
+
+
+import static org.springframework.cloud.gateway.server.mvc.filter.BeforeFilterFunctions.uri;
+import static org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions.http;
+
+@Configuration
+public class BookingServiceRoutes {
+
+    @Value("${app.backend.booking-service}")
+    private String BOOKING_SERVICE_URL;
+
+    @Bean
+    public RouterFunction<ServerResponse> bookingRoutes() {
+        return GatewayRouterFunctions.route("booking-service")
+                .POST(RequestPredicates.path("/api/v1/booking"), http())
+                .before(uri(BOOKING_SERVICE_URL + "/api/v1/booking"))
+                .build();
+    }
+}
