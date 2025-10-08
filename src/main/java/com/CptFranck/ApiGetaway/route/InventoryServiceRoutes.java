6e41e31a8a22965de1e9ceb31915a6d9622101ba
CptFranck <francois.caillet71@gmail.com>
2025-10-08 17:28:16 +0200

    Add InventoryServiceRoutes

---
+++ src/main/java/com/CptFranck/ApiGetaway/route/InventoryServiceRoutes.java
@@ -0,0 +1,29 @@
+package com.CptFranck.ApiGetaway.route;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.function.RequestPredicates;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.ServerResponse;
+
+import static org.springframework.cloud.gateway.server.mvc.filter.BeforeFilterFunctions.uri;
+import static org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions.http;
+
+@Configuration
+public class InventoryServiceRoutes {
+
+    @Value("${app.backend.inventory-service}")
+    private String INVENTORY_SERVICE_URL;
+
+    @Bean
+    public RouterFunction<ServerResponse> inventoryRoutes() {
+        return GatewayRouterFunctions.route("inventory-service")
+                .GET(RequestPredicates.path("/api/v1/inventory/venue/{venueId}"), http())
+                .before(request -> uri(INVENTORY_SERVICE_URL + "api/v1/inventory/venue/" + request.pathVariable("venueId")).apply(request))
+                .GET(RequestPredicates.path("/api/v1/inventory/event/{eventId}"), http())
+                .before(request -> uri(INVENTORY_SERVICE_URL + "api/v1/inventory/venue/" + request.pathVariable("venueId")).apply(request))
+                .build();
+    }
+}
