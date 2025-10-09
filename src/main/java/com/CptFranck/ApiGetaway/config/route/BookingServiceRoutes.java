433ac6f1a3744759bc6c0fe020560d876c396404
CptFranck <francois.caillet71@gmail.com>
2025-10-09 14:38:23 +0200

    add to booking service circuitbreakerfilter for booking service

--- src/main/java/com/CptFranck/ApiGetaway/config/route/BookingServiceRoutes.java
+++ src/main/java/com/CptFranck/ApiGetaway/config/route/BookingServiceRoutes.java
@@ -1,13 +1,17 @@
 package com.CptFranck.ApiGetaway.config.route;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.server.mvc.filter.CircuitBreakerFilterFunctions;
 import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.servlet.function.RequestPredicates;
 import org.springframework.web.servlet.function.RouterFunction;
 import org.springframework.web.servlet.function.ServerResponse;
 
+
+import java.net.URI;
 
 import static org.springframework.cloud.gateway.server.mvc.filter.BeforeFilterFunctions.uri;
 import static org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions.http;
@@ -26,6 +30,14 @@ public class BookingServiceRoutes {
         return GatewayRouterFunctions.route("booking-service")
                 .POST(RequestPredicates.path("/api/v1/booking"), http())
                 .before(uri(BOOKING_SERVICE_URL + "/api/v1/booking"))
+                .filter(CircuitBreakerFilterFunctions.circuitBreaker("bookingServiceCircuitBreaker", URI.create("forward:/fallbackRoute")))
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> fallbackRoute() {
+        return GatewayRouterFunctions.route("fallbackRoute")
+                .POST("/fallbackRoute", request -> ServerResponse.status(HttpStatus.SERVICE_UNAVAILABLE) .body("Booking service is down"))
                 .build();
     }
 }
