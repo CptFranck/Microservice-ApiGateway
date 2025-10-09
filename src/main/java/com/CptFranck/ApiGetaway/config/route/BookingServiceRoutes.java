031572756c808fec831f789278fb9e9ed4a7cc20
CptFranck <francois.caillet71@gmail.com>
2025-10-09 15:27:03 +0200

    add bookingServiceApiDocs

--- src/main/java/com/CptFranck/ApiGetaway/config/route/BookingServiceRoutes.java
+++ src/main/java/com/CptFranck/ApiGetaway/config/route/BookingServiceRoutes.java
@@ -14,6 +14,7 @@ import org.springframework.web.servlet.function.ServerResponse;
 import java.net.URI;
 
 import static org.springframework.cloud.gateway.server.mvc.filter.BeforeFilterFunctions.uri;
+import static org.springframework.cloud.gateway.server.mvc.filter.FilterFunctions.setPath;
 import static org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions.http;
 
 @Configuration
@@ -29,7 +30,8 @@ public class BookingServiceRoutes {
     public RouterFunction<ServerResponse> bookingRoutes() {
         return GatewayRouterFunctions.route("booking-service")
                 .POST(RequestPredicates.path("/api/v1/booking"), http())
-                .before(uri(BOOKING_SERVICE_URL + "/api/v1/booking"))
+                .before(uri(BOOKING_SERVICE_URL))
+                .filter(setPath("/api/v1/booking"))
                 .filter(CircuitBreakerFilterFunctions.circuitBreaker("bookingServiceCircuitBreaker", URI.create("forward:/fallbackRoute")))
                 .build();
     }
@@ -38,6 +40,15 @@ public class BookingServiceRoutes {
     public RouterFunction<ServerResponse> fallbackRoute() {
         return GatewayRouterFunctions.route("fallbackRoute")
                 .POST("/fallbackRoute", request -> ServerResponse.status(HttpStatus.SERVICE_UNAVAILABLE) .body("Booking service is down"))
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> bookingServiceApiDocs() {
+        return GatewayRouterFunctions.route("booking-service-api-docs")
+                .route(RequestPredicates.path("/docs/booking-service/v3/api-docs"), http())
+                .before(uri(BOOKING_SERVICE_URL))
+                .filter(setPath("/v3/api-docs"))
                 .build();
     }
 }
