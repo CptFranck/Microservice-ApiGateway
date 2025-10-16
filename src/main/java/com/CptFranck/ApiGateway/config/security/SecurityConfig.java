757de58b048793d37e5bdd1fff386201acb1d5fd
CptFranck <francois.caillet71@gmail.com>
2025-10-16 14:50:57 +0200

    update SecurityConfig for spring cloud gateway webflux architecture

--- src/main/java/com/CptFranck/ApiGateway/config/security/SecurityConfig.java
+++ src/main/java/com/CptFranck/ApiGateway/config/security/SecurityConfig.java
@@ -3,13 +3,14 @@ package com.CptFranck.ApiGateway.config.security;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
+@EnableWebFluxSecurity
 public class SecurityConfig {
 
 
@@ -20,17 +21,34 @@ public class SecurityConfig {
     @Value("${security.excluded.urls}")
     private String[] excludedUrls;
 
+
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        return httpSecurity.authorizeHttpRequests(authorizeRequests -> authorizeRequests
-                                .requestMatchers(excludedUrls).permitAll()
-                                .anyRequest().authenticated())
-                .oauth2ResourceServer(oauth -> oauth.jwt(Customizer.withDefaults()))
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        return http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(exchanges -> exchanges
+                        .pathMatchers(excludedUrls).permitAll()
+                        .anyExchange().authenticated())
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwtSpec -> jwtSpec.jwtDecoder(jwtDecoder())))
                 .build();
     }
 
     @Bean
-    public JwtDecoder jwtDecoder() {
-        return NimbusJwtDecoder.withJwkSetUri(jwkSetUri).build();
+    public ReactiveJwtDecoder jwtDecoder() {
+        return NimbusReactiveJwtDecoder.withJwkSetUri(jwkSetUri).build();
     }
+
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+//        return httpSecurity.authorizeHttpRequests(authorizeRequests -> authorizeRequests
+//                                .requestMatchers(excludedUrls).permitAll()
+//                                .anyRequest().authenticated())
+//                .oauth2ResourceServer(oauth -> oauth.jwt(Customizer.withDefaults()))
+//                .build();
+//    }
+//
+//    @Bean
+//    public JwtDecoder jwtDecoder() {
+//        return NimbusJwtDecoder.withJwkSetUri(jwkSetUri).build();
+//    }
 }
