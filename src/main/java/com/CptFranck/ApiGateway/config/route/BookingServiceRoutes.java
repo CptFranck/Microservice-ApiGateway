9acc67a8d0781ee116981c4ab181d30a8f8b88dc
CptFranck <francois.caillet71@gmail.com>
2025-10-16 14:56:33 +0200

    update BookingServiceRoutes to webflux architecture

--- src/main/java/com/CptFranck/ApiGateway/config/route/BookingServiceRoutes.java
+++ src/main/java/com/CptFranck/ApiGateway/config/route/BookingServiceRoutes.java
@@ -1,21 +1,10 @@
 package com.CptFranck.ApiGateway.config.route;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.gateway.server.mvc.filter.CircuitBreakerFilterFunctions;
-import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.servlet.function.RequestPredicates;
-import org.springframework.web.servlet.function.RouterFunction;
-import org.springframework.web.servlet.function.ServerResponse;
-
-
-import java.net.URI;
-
-import static org.springframework.cloud.gateway.server.mvc.filter.BeforeFilterFunctions.uri;
-import static org.springframework.cloud.gateway.server.mvc.filter.FilterFunctions.setPath;
-import static org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions.http;
 
 @Configuration
 public class BookingServiceRoutes {
@@ -27,28 +16,49 @@ public class BookingServiceRoutes {
     }
 
     @Bean
-    public RouterFunction<ServerResponse> bookingRoutes() {
-        return GatewayRouterFunctions.route("booking-service")
-                .POST(RequestPredicates.path("/api/v1/booking"), http())
-                .before(uri(BOOKING_SERVICE_URL))
-                .filter(setPath("/api/v1/booking"))
-                .filter(CircuitBreakerFilterFunctions.circuitBreaker("bookingServiceCircuitBreaker", URI.create("forward:/fallbackRoute")))
+    public RouteLocator bookingRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("booking-service", r -> r
+                        .path("/api/v1/booking")
+                        .filters(f -> f.circuitBreaker(config -> config
+                                .setName("bookingServiceCircuitBreaker")
+                                .setFallbackUri("forward:/fallbackRoute")))
+                        .uri(BOOKING_SERVICE_URL))
+
+                .route("booking-api-docs", r -> r
+                        .path("/docs/booking-service/v3/api-docs")
+                        .filters(f -> f.rewritePath("/docs/booking-service/v3/api-docs", "/v3/api-docs"))
+                        .uri(BOOKING_SERVICE_URL))
+
+                .route("fallback-route", r -> r
+                        .path("/fallbackRoute")
+                        .uri("forward:/fallbackRoute"))
                 .build();
     }
 
-    @Bean
-    public RouterFunction<ServerResponse> fallbackRoute() {
-        return GatewayRouterFunctions.route("fallbackRoute")
-                .POST("/fallbackRoute", request -> ServerResponse.status(HttpStatus.SERVICE_UNAVAILABLE) .body("Booking service is down"))
-                .build();
-    }
-
-    @Bean
-    public RouterFunction<ServerResponse> bookingServiceApiDocs() {
-        return GatewayRouterFunctions.route("booking-service-api-docs")
-                .route(RequestPredicates.path("/docs/booking-service/v3/api-docs"), http())
-                .before(uri(BOOKING_SERVICE_URL))
-                .filter(setPath("/v3/api-docs"))
-                .build();
-    }
+//    @Bean
+//    public RouterFunction<ServerResponse> bookingRoutes() {
+//        return GatewayRouterFunctions.route("booking-service")
+//                .POST(RequestPredicates.path("/api/v1/booking"), http())
+//                .before(uri(BOOKING_SERVICE_URL))
+//                .filter(setPath("/api/v1/booking"))
+//                .filter(CircuitBreakerFilterFunctions.circuitBreaker("bookingServiceCircuitBreaker", URI.create("forward:/fallbackRoute")))
+//                .build();
+//    }
+//
+//    @Bean
+//    public RouterFunction<ServerResponse> fallbackRoute() {
+//        return GatewayRouterFunctions.route("fallbackRoute")
+//                .POST("/fallbackRoute", request -> ServerResponse.status(HttpStatus.SERVICE_UNAVAILABLE) .body("Booking service is down"))
+//                .build();
+//    }
+//
+//    @Bean
+//    public RouterFunction<ServerResponse> bookingServiceApiDocs() {
+//        return GatewayRouterFunctions.route("booking-service-api-docs")
+//                .route(RequestPredicates.path("/docs/booking-service/v3/api-docs"), http())
+//                .before(uri(BOOKING_SERVICE_URL))
+//                .filter(setPath("/v3/api-docs"))
+//                .build();
+//    }
 }
