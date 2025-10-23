c24f37211fe14d336f792bcccb6d169caed8cd11
CptFranck <francois.caillet71@gmail.com>
2025-10-23 17:30:33 +0200

    update WebConfig

--- src/main/java/com/CptFranck/ApiGateway/config/WebConfig.java
+++ src/main/java/com/CptFranck/ApiGateway/config/WebConfig.java
@@ -2,13 +2,14 @@ package com.CptFranck.ApiGateway.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.CorsWebFilter;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
-import java.util.Arrays;
 import java.util.List;
 
+@Configuration
 public class WebConfig {
 
     private static final List<String> HEADERS_ACCEPTED = List.of(
@@ -18,20 +19,20 @@ public class WebConfig {
             "Origin",
             "X-Requested-With"
     );
-    private static final List<String> METHODS_REST = List.of("GET", "POST");
+    private static final List<String> METHODS_REST = List.of("GET","POST","OPTIONS");
 
-    private static String[] ALLOWED_ORIGINS;
+    private static List<String> ALLOWED_ORIGINS;
 
     public WebConfig(@Value("${security.cors.allowed-origins}") String[] allowedOrigins) {
-        ALLOWED_ORIGINS = allowedOrigins;
+        ALLOWED_ORIGINS = List.of(allowedOrigins);
     }
 
     @Bean
     public CorsWebFilter corsWebFilter() {
         CorsConfiguration corsConfig = new CorsConfiguration();
-        corsConfig.addAllowedOriginPattern(Arrays.toString(ALLOWED_ORIGINS));
-        corsConfig.addAllowedMethod(String.valueOf(METHODS_REST));
-        corsConfig.addAllowedHeader(String.valueOf(HEADERS_ACCEPTED));
+        corsConfig.setAllowedOriginPatterns(ALLOWED_ORIGINS);
+        corsConfig.setAllowedMethods(METHODS_REST);
+        corsConfig.setAllowedHeaders(HEADERS_ACCEPTED);
         corsConfig.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
