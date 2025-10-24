b37a54f0f9432e1c4514ba4a17188c8df9011fa0
CptFranck <francois.caillet71@gmail.com>
2025-10-24 12:36:05 +0200

    remove unused code

--- src/main/java/com/CptFranck/ApiGateway/config/route/BookingServiceRoutes.java
+++ src/main/java/com/CptFranck/ApiGateway/config/route/BookingServiceRoutes.java
@@ -35,30 +35,4 @@ public class BookingServiceRoutes {
                         .uri("forward:/fallbackRoute"))
                 .build();
     }
-
-//    @Bean
-//    public RouterFunction<ServerResponse> bookingRoutes() {
-//        return GatewayRouterFunctions.route("booking-service")
-//                .POST(RequestPredicates.path("/api/v1/booking"), http())
-//                .before(uri(BOOKING_SERVICE_URL))
-//                .filter(setPath("/api/v1/booking"))
-//                .filter(CircuitBreakerFilterFunctions.circuitBreaker("bookingServiceCircuitBreaker", URI.create("forward:/fallbackRoute")))
-//                .build();
-//    }
-//
-//    @Bean
-//    public RouterFunction<ServerResponse> fallbackRoute() {
-//        return GatewayRouterFunctions.route("fallbackRoute")
-//                .POST("/fallbackRoute", request -> ServerResponse.status(HttpStatus.SERVICE_UNAVAILABLE) .body("Booking service is down"))
-//                .build();
-//    }
-//
-//    @Bean
-//    public RouterFunction<ServerResponse> bookingServiceApiDocs() {
-//        return GatewayRouterFunctions.route("booking-service-api-docs")
-//                .route(RequestPredicates.path("/docs/booking-service/v3/api-docs"), http())
-//                .before(uri(BOOKING_SERVICE_URL))
-//                .filter(setPath("/v3/api-docs"))
-//                .build();
-//    }
 }
