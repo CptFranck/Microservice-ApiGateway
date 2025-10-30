b1243f815c2ba778ff1937fda95df25b235aa58d
CptFranck <francois.caillet71@gmail.com>
2025-10-30 16:55:49 +0100

    add oauth2Login default conf

--- src/main/java/com/CptFranck/ApiGateway/config/security/SecurityConfig.java
+++ src/main/java/com/CptFranck/ApiGateway/config/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.CptFranck.ApiGateway.config.WebConfig;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder;
@@ -48,6 +49,7 @@ public class SecurityConfig {
                                 "/api/v1/inventory"
                         ).authenticated()
                         .anyExchange().authenticated())
+                .oauth2Login(Customizer.withDefaults())
                 .oauth2ResourceServer(oauth2 -> oauth2
                         .jwt(jwtSpec -> jwtSpec
                                 .jwtDecoder(jwtDecoder())
